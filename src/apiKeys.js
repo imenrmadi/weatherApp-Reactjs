281e581b01c2381c9f02f5deae25a4e0ed563ffa
imenrmadi <rmadi.imen@gmail.com>
2026-05-07 00:30:30 +0200

    test webhook jenkins

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://openweathermap.org/api & signup to get your free API key
+// Replace "{Your API Key Here}" with your actual API key
+module.exports = {
+  key: "aeca6970dee28d6201ff0be0181f64ce",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
